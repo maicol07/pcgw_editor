8d820ce5e6c2b104615f582c4ba2bc0497636976
Maicol Battistini <maicolbattistini@live.it>
2026-03-04 11:44:13 +0100

    chore: Remove unused wikiparser-node import from test setup.

--- tests/setup.ts
+++ tests/setup.ts
@@ -3,7 +3,6 @@ import { config } from '@vue/test-utils';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 import Tooltip from 'primevue/tooltip';
-import 'wikiparser-node/bundle/bundle-lsp.min.js';
 
 // Mock localStorage if it's broken in the environment
 if (typeof window !== 'undefined') {
