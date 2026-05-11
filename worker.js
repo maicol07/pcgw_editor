91a898061ec4eb309a49b338fb94091c59396926
Maicol Battistini <maicolbattistini@live.it>
2026-05-11 19:46:55 +0200

    fix: add missing headers to CORS policy in worker.js

--- worker.js
+++ worker.js
@@ -15,7 +15,7 @@ export default {
         const corsHeaders = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Headers': 'Content-Type, api-user-agent, User-Agent, X-Requested-With',
         };
 
         if (request.method === 'OPTIONS') {
